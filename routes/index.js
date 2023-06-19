49d4a1fe468a16f34f0bd209696ee035d1378483
AYcrown77 <aycrown77@gmail.com>
2023-06-19 07:45:22 -0700

    Solve task 5 & 6

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
 
 const router = express.Router();
 
@@ -10,8 +13,11 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 // the post Routes is here
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
