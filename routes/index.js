883559927466f9224f1580966024e66d77c05d0c
AYcrown77 <aycrown77@gmail.com>
2023-06-19 07:23:54 -0700

    Solve task 4

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const router = express.Router();
 // the get Routes is here
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 // the post Routes is here
 router.post('/users', UsersController.postNew);
