e678fe92977f115cdbf49f4a15b7db36ad8c6ff4
AYcrown77 <aycrown77@gmail.com>
2023-06-19 06:55:17 -0700

    Solve task 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 // the get Routes is here
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// the post Routes is here
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
